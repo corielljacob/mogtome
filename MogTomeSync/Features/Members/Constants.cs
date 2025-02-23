e32826c5f23f3a3d015d8e586d6176d69b92dde7
corielljacob <32170426+corielljacob@users.noreply.github.com>
2025-02-23 15:30:15 -0600

    Remove sync, add total member count for search, cleanup some css

--- MogTomeSync/Features/Members/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace fc_tracker_api.Features.Members
-{
-    public class Constants
-    {
-        public readonly static string KupoLifeId = "9229142273877457819";
-        public readonly static string ConnectionStringId = "FC_Tracker_Connection_String";
-    }
-}
