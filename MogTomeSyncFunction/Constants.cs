cbeb84f885951afca5c89de53e2ca86860d01deb
corielljacob <32170426+corielljacob@users.noreply.github.com>
2025-02-23 22:39:43 -0600

    Remove sync function files from repo. Add filter by rank

--- MogTomeSyncFunction/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MogTomeSyncFunction
-{
-    public class Constants
-    {
-        public readonly static string KupoLifeId = "9229142273877457819";
-        public readonly static string ConnectionStringId = "FC_Tracker_Connection_String";
-    }
-}
